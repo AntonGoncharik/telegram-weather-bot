a22e6959193d1c20a2a0fd074a41414174157543
AntonGoncharik <ant.goncharik@gmail.com>
2020-12-24 23:49:11 +0300

    added controller

---
+++ controller.js
@@ -0,0 +1,38 @@
+const userModel = require('./model');
+
+const Controller = {
+    getUser: async (telegramId) => {
+        try {
+            const user = await userModel.find({ telegramId });
+            return user;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    addUser: async (data) => {
+        try {
+            const user = await new userModel(data).save();
+            return user;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    updateUser: async (telegramId, data) => {
+        try {
+            const user = await userModel.findOneAndUpdate({ telegramId }, data, { new: true, });
+            return user;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    removeUser: async (telegramId) => {
+        try {
+            const user = await userModel.findOneAndDelete({ telegramId });
+            return user;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+};
+
+module.exports = Controller;
