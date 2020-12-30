908472ba1087bc14f02da882be2424128fe4e15c
AntonGoncharik <ant.goncharik@gmail.com>
2020-12-30 12:46:01 +0300

    test

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   telegramApiToken: '1295066323:AAGSRE9ikE-s16k4vRfqo3pD-sZ3uoBcdHE',
   weatherApiToken: '28aef5bb16b05adaa6fa23982cf21401',
   dbURL: 'mongodb+srv://admin:cNJ08Kc1J5pN05TT@cluster0.72oi3.mongodb.net/weather?retryWrites=true&w=majority',
-  schedule: '* * 7 * * *',
+  schedule: '1 * * * * *',
   coefficientPressure: 1.33322,
   dbOptions: {
     useUnifiedTopology: true,
