6380973db596b61d1b4836ef88382d1f0964d02c
AntonGoncharik <ant.goncharik@gmail.com>
2020-12-31 16:19:39 +0300

    test send 4

--- helper.js
+++ helper.js
@@ -20,26 +20,26 @@ const helper = {
         ${strings.getString('uvi', user.language)}: ${weather.current.uvi}
         ${strings.getString('description', user.language)}: ${weather.current.weather.map((elem) => elem.description).toString()}`);
 
-        weatherTextList.push(`${strings.getString('forecastWeather', user.language)}:`);
+        // weatherTextList.push(`${strings.getString('forecastWeather', user.language)}:`);
 
-        weather.hourly.forEach((item, index) => {
-            if (index < 25 && index > 0) {
-                weatherTextList.push(`
-                *
-                ${strings.getString('date', user.language)}: ${new Date(item.dt * 1000).toJSON().slice(0, 10).split('-').reverse().join('/')} ${new Date(item.dt * 1000).toLocaleTimeString('ru-RU', { hour: '2-digit', minute: '2-digit', hour12: false })}
-                ${strings.getString('temp', user.language)}: ${item.temp} °C
-                ${strings.getString('tempFeelsLike', user.language)}: ${item.feels_like} °C
-                ${strings.getString('pressure', user.language)}: ${(item.pressure / config.coefficientPressure).toFixed()} ${strings.getString('mmHg', user.language)}
-                ${strings.getString('humidity', user.language)}: ${item.humidity} %
-                ${strings.getString('clouds', user.language)}: ${item.clouds} %
-                ${strings.getString('windSpeed', user.language)}: ${item.wind_speed} ${strings.getString('metreSec', user.language)}
-                ${strings.getString('windDeg', user.language)}: ${helper.getDirection(item.wind_deg, user.language)}
-                ${strings.getString('visibility', user.language)}: ${item.visibility} ${strings.getString('metres', user.language)}
-                ${strings.getString('uvi', user.language)}: ${item.uvi}
-                ${strings.getString('description', user.language)}: ${item.weather.map((elem) => elem.description).toString()}
-                `);
-            }
-        });
+        // weather.hourly.forEach((item, index) => {
+        //     if (index < 25 && index > 0) {
+        //         weatherTextList.push(`
+        //         *
+        //         ${strings.getString('date', user.language)}: ${new Date(item.dt * 1000).toJSON().slice(0, 10).split('-').reverse().join('/')} ${new Date(item.dt * 1000).toLocaleTimeString('ru-RU', { hour: '2-digit', minute: '2-digit', hour12: false })}
+        //         ${strings.getString('temp', user.language)}: ${item.temp} °C
+        //         ${strings.getString('tempFeelsLike', user.language)}: ${item.feels_like} °C
+        //         ${strings.getString('pressure', user.language)}: ${(item.pressure / config.coefficientPressure).toFixed()} ${strings.getString('mmHg', user.language)}
+        //         ${strings.getString('humidity', user.language)}: ${item.humidity} %
+        //         ${strings.getString('clouds', user.language)}: ${item.clouds} %
+        //         ${strings.getString('windSpeed', user.language)}: ${item.wind_speed} ${strings.getString('metreSec', user.language)}
+        //         ${strings.getString('windDeg', user.language)}: ${helper.getDirection(item.wind_deg, user.language)}
+        //         ${strings.getString('visibility', user.language)}: ${item.visibility} ${strings.getString('metres', user.language)}
+        //         ${strings.getString('uvi', user.language)}: ${item.uvi}
+        //         ${strings.getString('description', user.language)}: ${item.weather.map((elem) => elem.description).toString()}
+        //         `);
+        //     }
+        // });
 
         return weatherTextList;
     },
